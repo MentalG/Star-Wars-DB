f12e7919e9ad46a48b9b53951393a2f7836887ed
Illia Hromov <i.hromov@stollerukraine.com>
2019-10-21 15:43:59 +0300

    project structure & API services

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
+function App () {
+   return (
     <div className="app">
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,4 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+//services
+import SwapiService from './services/swapi-service'
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+    people.forEach((person) => {
+        console.log(person);
+    });
+})
+
+swapi.getPerson(3).then((person) => {
+    console.log(person);
+})
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
